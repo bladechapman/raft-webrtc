509fbb17a71ecc3fadaf9a96394a606e63660a6d
blade-chapman <blade_chapman@apple.com>
2020-04-22 17:54:49 -0700

    minor lib enhancement

--- raft-core/lib.ts
+++ raft-core/lib.ts
@@ -11,6 +11,9 @@ type TFailedResult<U> = {
 
 export type TResult<T, U> = TOkResult<T> | TFailedResult<U>;
 export const Result = {
-    okResult: function<T>(data: T) { return { data } as TOkResult<T> },
-    failedResult: function<U>(data: U) { return { data } as TFailedResult<U> }
+    okResult: function<T>(data: T): TOkResult<T> { return { type: "Ok", data } },
+    failedResult: function<U>(data: U): TFailedResult<U> { return { type: "Failed", data } },
+
+    isOk: function<T, U>(result: TResult<T, U>): result is TOkResult<T> { return result.type === "Ok"; },
+    isFailed: function<T, U>(result: TResult<T, U>): result is TFailedResult<U> { return result.type === "Failed"; }
 }
