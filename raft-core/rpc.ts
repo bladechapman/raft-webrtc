0fd3e53c3ace1b6535bed7d03b202a3f4c8deda4
blade-chapman <blade_chapman@apple.com>
2020-05-09 11:26:47 -0700

    wip1

--- raft-core/rpc.ts
+++ raft-core/rpc.ts
@@ -2,134 +2,175 @@ import { RaftNodeId } from './raftNode';
 import { TLogEntry } from './log';
 import { Result, TResult } from './lib';
 
-const rpcGroup: {
-    [memberId: number]: {  
-        memberId: number,
-        delegate: any,
-        callIds: {
-            [callId: string]: [any /* resolve */, any /* reject */]
-        }
-    }
-} = {};
 
-export function rpcInvoke(invokerId, receiverId, method, args) {
-    const callId = Math.random();   // TODO: improve this
-    const invokePayload = {
-        method,
-        args: JSON.stringify(args),
-        invokerId,
-        callId,
-        __invoke: true
+type Parameters<T> = T extends (...args: infer U) => any ? U : never;
+type ReturnType<T> = T extends (...args: any[]) => infer U ? U : never;
+
+type TInvokePayload<T extends { [key: string]: Function }> = {
+    method: keyof T,
+    args: string,
+    invokerId: number,
+    callId: number
+};
+
+type TRespondPayload = {
+    result: string,
+    invokerId: number,
+    callId: number,
+};
+
+function isInvokePayload<T extends { [key: string]: Function }>(
+    p: TInvokePayload<T> | TRespondPayload
+): p is TInvokePayload<T> {
+    return (p as TInvokePayload<T>).method !== undefined;
+}
+
+export function useRpc<T extends { [key: string]: Function }>(): [
+    (delegate: T) => number,
+    (i: number, r: number, m: keyof T, a: Parameters<T[keyof T]>) => Promise<ReturnType<T[keyof T]>>
+]
+{
+    type TRpcGroupMember = {
+        memberId: number,
+        delegate: T,
+        callIds: {
+            [callId: string]: [
+                (v: ReturnType<T[keyof T]>) => void,    // res
+                any                                     // rej
+            ]
+        }
     };
 
-    let responsePromise = new Promise((res, rej) => {
-        // TODO: Add timeout?
-        rpcGroup[invokerId].callIds[callId] = [res, rej];
+    const rpcGroup: {
+        [memberId: number]: TRpcGroupMember
+    } = {};
+
+
+    function rpcRegister(delegate: T) {
+        const id = Math.random();
+        rpcGroup[id] = {
+            memberId: id,
+            delegate,
+            callIds: {}
+        }
+
+        return id;
+    }
+
+    function rpcHandleResponse(responsePayload: TRespondPayload) {
+        const {
+            result,
+            invokerId,
+            callId
+        } = responsePayload;
+
+        // extract the result from the payload
+        const [res, rej] = rpcGroup[invokerId].callIds[callId];
+        delete rpcGroup[invokerId].callIds[callId];
+
+        // resolve the promise with the result
+        const parsedResult = JSON.parse(result) as ReturnType<T[keyof T]>;  // TODO: make this stricter
+        res(parsedResult);
+    }
+
+    async function rpcRespond(receiverId, senderPayload: TInvokePayload<T>) {
+        const {
+            method,
+            args: argsString,
+            invokerId,
+            callId
+        } = senderPayload;
+
+        // invoke whatever method is requested
+        const args = JSON.parse(argsString);
+        const result = await rpcGroup[receiverId].delegate[method].apply(null, args);
+
+        const responsePayload = {
+            result: JSON.stringify(result),
+            invokerId,
+            callId,
+        };
+
+        // send to senderId with responsePayload
+        fakeSend(invokerId, responsePayload);
+    }
+
+    async function rpcReceive(
+        receiverId: number,
+        senderPayload: TInvokePayload<T> | TRespondPayload
+    ) {
+        
+        if (isInvokePayload(senderPayload)) await rpcRespond(receiverId, senderPayload);
+        else rpcHandleResponse(senderPayload);
+    }
+
+    function rpcInvoke(
+        invokerId: number,
+        receiverId: number,
+        method: keyof T,
+        args: Parameters<T[keyof T]>
+    ): Promise<ReturnType<T[keyof T]>> {
+        const callId = Math.random();   // TODO: improve this
+        const invokePayload: TInvokePayload<T> = {
+            method,
+            args: JSON.stringify(args),
+            invokerId,
+            callId,
+        };
+
+        return new Promise((res, rej) => {
+            // TODO: Add timeout?
+            rpcGroup[invokerId].callIds[callId] = [res, rej];
+
+            // Send to receiverId with invokePayload
+            fakeSend(receiverId, invokePayload);
+        });
+    }
+
+    function fakeSend(
+        receiverId: number,
+        payload: TInvokePayload<T> | TRespondPayload
+    ) {
+        setTimeout(() => {
+            fakeReceive(receiverId, payload);
+        }, Math.random() * 100 + 100);
+    }
+
+    function fakeReceive(
+        id: number,
+        payload: TInvokePayload<T> | TRespondPayload
+    ) {
+        rpcReceive(id, payload);
+    }
+
+
+    return [rpcRegister, rpcInvoke];
+}
+
+
+
+
+
+if (require.main === module) {
+    type protocol = {
+        hello: (x: string) => string,
+        add: (y: number, z: number) => number
+    }
+
+    const [rpcRegister, rpcInvoke] = useRpc<protocol>();
+
+    const idA = rpcRegister({
+        hello: (x) => { return `A says ${x}`; },
+        add: (x, y) => { return x + y }
     });
 
-    // Send to receiverId with invokePayload
-    fakeSend(receiverId, invokePayload);
+    const idB = rpcRegister({
+        hello: (x) => { return `B says ${x}`; },
+        add: (x, y) => { return x + y }
+    });
 
-    return responsePromise;
+
+    rpcInvoke(idA, idB, "hello", [3, 4]).then((v) => {
+        console.log(v);
+    });
 }
-
-// export function rpcBroadcast(invokerId, method, args) {
-//     return Promise.all(
-//         Object.values(rpcGroup).map(rpcMember => 
-//             rpcInvoke(invokerId, rpcMember.memberId, method, args)
-//         )
-//     );
-// }
-
-async function rpcReceive(receiverId, senderPayload) {
-    const isInvocation = senderPayload.__invoke === true;
-    if (isInvocation) await rpcRespond(receiverId, senderPayload);
-    else rpcHandleResponse(senderPayload);
-}
-
-const delegate = {};
-async function rpcRespond(receiverId, senderPayload) {
-    const {
-        method,
-        args: argsString,
-        invokerId,
-        callId
-    } = senderPayload;
-
-    // invoke whatever method is requested
-    const args = JSON.parse(argsString);
-    const result = await rpcGroup[receiverId].delegate[method].apply(null, args);
-
-    const responsePayload = {
-        result,
-        invokerId,
-        callId,
-        __response: true
-    };
-
-    // send to senderId with responsePayload
-    fakeSend(invokerId, responsePayload);
-}
-
-function rpcHandleResponse(responsePayload) {
-    const {
-        result,
-        invokerId,
-        callId
-    } = responsePayload;
-
-    // extract the result from the payload
-    const [res, rej] = rpcGroup[invokerId].callIds[callId];
-    delete rpcGroup[invokerId].callIds[callId];
-
-    // resolve the promise with the result
-    res(result);
-}
-
-
-export function rpcRegister(delegate) {
-    const id = Math.random();
-    rpcGroup[id] = {
-        memberId: id,
-        delegate,
-        callIds: {}
-    }
-
-    return [id, rpcGroup];
-}
-
-
-function fakeSend(receiverId, payload) {
-    setTimeout(() => {
-        fakeReceive(receiverId, payload);
-    }, Math.random() * 100 + 100);
-}
-
-function fakeReceive(id, payload) {
-    rpcReceive(id, payload);
-}
-
-// if (require.main === module) {
-//     const idA = rpcRegister({
-//         hello: (x) => {
-//             console.log(`Hello ${x}!`);
-//             return 'Goodbye';
-//         }
-//     });
-
-//     const idB = rpcRegister({
-//         speak: (y) => {
-//             console.log(`Bark ${y}`);
-//             return true;
-//         }
-//     });
-
-
-//     rpcInvoke(idA, idB, 'speak', ['test']).then((r) => {
-//         console.log(r);
-
-//         rpcInvoke(idB, idA, 'hello', ['A']).then((r) => {
-//             console.log(r);
-//         });
-//     });
-// }
